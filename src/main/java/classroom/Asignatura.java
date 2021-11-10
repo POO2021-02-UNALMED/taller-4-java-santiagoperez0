d96b01c68cee840e7b50c9a171addeccbcc252e2
santiagoperez0 <jperezpi@unal.edu.co>
2021-11-09 22:00:13 -0500

    sssss

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -1,33 +1,27 @@
 package classroom;
 
-public class Asignatura {
+public class Asignatura{
 
     String nombre;
-    String n = nombre;
     int codigoInterno;
     int codigoExterno;
 
-    Asignatura() {
-        this("Sin nombre");
-        this(0);
-    }
-
-    Asignatura(int codigoInterno) {
-        this("Sin nombre", codigoInterno, 0);
-    }
-
-    Asignatura(int codigoExterno) {
-        this("Sin nombre", 0, codigoExterno);
-    }
-
-    Asignatura(String nombre) {
-        this(nombre, 0, 0);
-    }
 
     Asignatura(String nombre, int codigoInterno, int codigoExterno) {
         this.nombre = nombre;
         this.codigoInterno = codigoInterno;
         this.codigoExterno = codigoExterno;
+    }
+    Asignatura(int codigoExterno) {
+        this("Sin nombre", 0, codigoExterno);
+    }
+
+    Asignatura() {
+        this("Sin nombre");
+    }
+
+    Asignatura(String nombre) {
+        this(nombre, 0, 0);
     }
 
     void cambiarDatos(int codigoInterno, int codigoExterno, String nombre) {
@@ -36,12 +30,9 @@ public class Asignatura {
         this.nombre = nombre;
     }
 
-    void cambiarDatos(int codigoInterno) {
-        this.codigoInterno = codigoInterno;
-    }
 
     void cambiarDatos(int codigoExterno) {
-        this.codigoExterno = codigoExterno;
+        this.codigoExterno = (int) codigoExterno;
     }
 
     void cambiarDatos(String nombre) {
@@ -51,7 +42,7 @@ public class Asignatura {
     public void setCodigoInterno(int codigoInterno) {
         this.codigoInterno = codigoInterno;
     }
-    
+
     public void setCodigoInterno(double codigoInterno) {
         this.codigoInterno = (int) codigoInterno;
     }
